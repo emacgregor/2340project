cf27b4a481b8e6805d245430ea3872375ddf90c9
Hyland <hlwolleat@gmail.com>
2018-02-13 22:46:10 -0500

    Added Welcome and Main Screens with active buttons. Changed boot screen to the Welcome Screen.

--- app/src/main/java/edu/gatech/cs2340/app/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/app/MainActivity.java
@@ -1,11 +1,13 @@
 package edu.gatech.cs2340.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button logOutBtn = (Button) findViewById(R.id.button);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent welcomeScreen = new Intent(MainActivity.this, WelcomeScreen.class);
+                startActivity(welcomeScreen);
             }
         });
     }
--- app/src/main/java/edu/gatech/cs2340/app/WelcomeScreen.java
+++ app/src/main/java/edu/gatech/cs2340/app/WelcomeScreen.java
@@ -1,11 +1,13 @@
 package edu.gatech.cs2340.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -15,6 +17,8 @@ public class WelcomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_welcome_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button loginBtn = (Button) findViewById(R.id.log_in_btn);
+        Button regBtn = (Button) findViewById(R.id.reg_btn);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +26,20 @@ public class WelcomeScreen extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent loginScreen = new Intent(WelcomeScreen.this, LoginScreen.class);
+                startActivity(loginScreen);
+            }
+        });
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent registrationScreen = new Intent(WelcomeScreen.this, RegistrationScreen.class);
+                startActivity(registrationScreen);
             }
         });
     }
